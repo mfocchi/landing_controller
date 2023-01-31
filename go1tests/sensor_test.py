872105824957255645a55a3a1181cdd3f86487fc
froscia <francesco.roscia@iit.it>
2023-01-31 11:49:04 +0100

    time analysis in sensor test

--- go1tests/sensor_test.py
+++ go1tests/sensor_test.py
@@ -10,7 +10,7 @@ np.set_printoptions(linewidth=np.inf,  # number of characters per line befor new
 
 from base_controllers.quadruped_controller import Controller
 import rospy as ros
-
+import time
 
 from base_controllers.utils.pidManager import PidManager
 
@@ -20,18 +20,33 @@ ROBOT_NAME = 'go1'                         # go1, solo, (aliengo)
 if __name__ == '__main__':
     p = Controller(ROBOT_NAME)
     p.startController(additional_args=['go0_conf:=standDown'])
+    uk_time = np.full(p.time_log.shape, np.nan)
+    sc_time = np.full(p.time_log.shape, np.nan)
     p.pid = PidManager(p.joint_names)
     p.pid.setPDjoints(np.zeros(p.robot.na),
                       np.zeros(p.robot.na),
                       np.zeros(p.robot.na))
+    q_des = p.q_des.copy()
+    qd_des = np.zeros(12)
+    tau_ffwd = np.zeros(12)
 
     try:
 
-        while not ros.is_shutdown():
+        while p.time < 40.:
+            start = time.time()
             p.updateKinematics()
-            p.imu_utils.compute_lin_vel(p.W_base_lin_acc, p.loop_time)
+            tau_ffwdg = p.gravityCompensation()
+            uk_time[p.log_counter] = time.time() - start
 
-            p.send_command(np.zeros(p.robot.na), np.zeros(p.robot.na), np.zeros(p.robot.na))
+            # p.imu_utils.compute_lin_vel(p.W_base_lin_acc, p.loop_time)
+            # p.w_p_b_legOdom, p.w_v_b_legOdom = p.leg_odom.estimate_base_wrt_world(p.contact_state,
+            #                                                                       p.quaternion,
+            #                                                                       p.q,
+            #                                                                       p.u.angPart(p.baseTwistW),
+            #                                                                       p.qd)
+            start = time.time()
+            p.send_command(q_des, qd_des, tau_ffwd)
+            sc_time[p.log_counter] = time.time() - start
     except (ros.ROSInterruptException, ros.service.ServiceException):
         ros.signal_shutdown("killed")
         p.deregister_node()
@@ -50,5 +65,15 @@ if __name__ == '__main__':
     plotCoM('position', 3, time_log=p.time_log.flatten(), basePoseW=p.basePoseW_log)
     # plotCoM('velocity', 4, time_log=p.time_log.flatten(), baseTwistW=p.baseTwistW_log)
     plotGRFs(6, time_log=p.time_log.flatten(), des_forces=p.grForcesW_des_log, act_forces=p.grForcesW_log)
+
+    import matplotlib.pyplot as plt
+
+    plt.figure()
+    plt.plot(p.time_log[:p.log_counter - 1], sc_time[:p.log_counter - 1] * 10e3)
+    plt.plot(p.time_log[:p.log_counter - 1], uk_time[:p.log_counter - 1] * 10e3)
+    plt.legend(['updateKinematics', 'send_command'])
+    plt.xlabel('time [s]')
+    plt.ylabel('computation time [ms]')
+    plt.title('Desktop PC, in Docker')
 
 
