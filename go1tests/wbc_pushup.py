b7255e382305cbedce0e085878cc0a35e1c3f4e1
froscia <francesco.roscia@iit.it>
2023-02-10 11:00:51 +0100

    cleaning

--- go1tests/wbc_pushup.py
+++ go1tests/wbc_pushup.py
@@ -20,6 +20,7 @@ robotName = "go1"
 test = {}
 test['duration'] = 300.
 test['stabilize'] = 10.
+test['usePid'] = 5.
 test['amp']  = np.array([0.0, 0., 0.05, 0., 0., 0.])
 test['freq'] = np.array([0.0, 0., 0.5, 0., 0., 0.])
 test['typeWBC'] = 'projection'
@@ -33,16 +34,11 @@ test['friction_coeffs'] = [0.8]*4
 
 if __name__ == '__main__':
     p = Controller(robotName)
-    UK_log =  np.zeros(conf.robot_params[p.robot_name]['buffer_size']) * np.nan
-    WBC_log = np.zeros(conf.robot_params[p.robot_name]['buffer_size']) * np.nan
-    pub_log = np.zeros(conf.robot_params[p.robot_name]['buffer_size']) * np.nan
     test['pulse'] = 2*np.pi*test['freq']
     test['pulse2'] = test['pulse']**2
     try:
 
-        #p.startController(world_name='slow.world', additional_args=['gui:=True', 'go0_conf:=standDown'])
-        p.startController(additional_args=['gui:=False', 'go0_conf:=standDown'])
-
+        p.startController( use_ground_truth_contacts=False)
         p.startupProcedure()  # overloaded method
         p.leg_odom.reset(np.hstack([0., 0., p.robot_height, p.quaternion, p.q]))
         p.w_p_b_legOdom, p.w_v_b_legOdom = p.leg_odom.estimate_base_wrt_world(p.contact_state,
@@ -50,11 +46,9 @@ if __name__ == '__main__':
                                                                               p.q,
                                                                               p.u.angPart(p.baseTwistW),
                                                                               p.qd)
-
         if test['typeWBC'] == 'qp':
             p.setWBCConstraints(test['normals'], test['friction_coeffs'])
 
-        p.updateKinematics()
         # Reset reference to actual value
 
         time_init = p.time.copy()
@@ -62,35 +56,40 @@ if __name__ == '__main__':
         # p.x0[:2] = 0
         # p.x0[3:] = 0
         p.x0 = np.zeros(6)
-        p.x0[2] = 0.240
+
         print('start!')
-        p.pid.setPDs(0.0, 0.0, 0.0)
+
+        changeController = True
+
         # Control loop
         while not ros.is_shutdown():
             # p.pr.enable()
             # update the kinematics
-            start_time = time.time()
-            p.w_p_b_legOdom, p.w_v_b_legOdom = p.leg_odom.estimate_base_wrt_world(p.contact_state,
-                                                                                  p.quaternion,
-                                                                                  p.q,
-                                                                                  p.u.angPart(p.baseTwistW),
-                                                                                  p.qd)
+            # p.w_p_b_legOdom, p.w_v_b_legOdom = p.leg_odom.estimate_base_wrt_world(p.contact_state,
+            #                                                                       p.quaternion,
+            #                                                                       p.q,
+            #                                                                       p.u.angPart(p.baseTwistW),
+            #                                                                       p.qd)
             p.updateKinematics()
             p.imu_utils.compute_lin_vel(p.W_base_lin_acc, p.loop_time)
-            UK_log[p.log_counter]= time.time()-start_time
             # Reference Generation
-            if p.time-time_init < test['stabilize']:
-                p.comPoseW_des = p.x0
-                p.comTwistW_des = np.zeros(6)
-                p.comAccW_des = np.zeros(6)
+            if p.time - time_init < test['usePid']:
+                p.tau_ffwd = p.gravityCompensation()
             else:
-                p.comPoseW_des = p.x0 + test['amp'] * np.sin( test['pulse']*(p.time-time_init-test['stabilize']) )
-                p.comTwistW_des = test['pulse']*test['amp'] * np.cos( test['pulse']*(p.time-time_init-test['stabilize']) )
-                p.comAccW_des = -test['pulse2'] * test['amp'] * np.sin( test['pulse']*(p.time-time_init-test['stabilize']) )
-            start_time = time.time()
+                if p.time-time_init < test['stabilize']:
+                    if changeController:
+                        p.x0 = p.comPoseW.copy()
+                        p.pid.setPDs(0.0, 0.0, 0.0)
+                        changeController = False
+                    p.comPoseW_des = p.x0
+                    p.comTwistW_des = np.zeros(6)
+                    p.comAccW_des = np.zeros(6)
+                else:
+                    p.comPoseW_des = p.x0 + test['amp'] * np.sin( test['pulse']*(p.time-time_init-test['stabilize']) )
+                    p.comTwistW_des = test['pulse']*test['amp'] * np.cos( test['pulse']*(p.time-time_init-test['stabilize']) )
+                    p.comAccW_des = -test['pulse2'] * test['amp'] * np.sin( test['pulse']*(p.time-time_init-test['stabilize']) )
 
-            p.tau_ffwd = p.WBC(p.comPoseW_des, p.comTwistW_des, p.comAccW_des, comControlled = True, type=test['typeWBC'])
-            WBC_log[p.log_counter]= time.time()-start_time
+                p.tau_ffwd = p.WBC(p.comPoseW_des, p.comTwistW_des, p.comAccW_des, comControlled = True, type=test['typeWBC'])
 
 
             # plot actual (green) and desired (blue) contact forces
@@ -126,14 +125,14 @@ if __name__ == '__main__':
         if conf.plotting:
 
 
-            plotCoM('position', 10, time_log=p.time_log.flatten(), des_basePoseW=p.comPoseW_des_log,
+            plotCoM('position', 0, time_log=p.time_log.flatten(), des_basePoseW=p.comPoseW_des_log,
                     basePoseW=p.comPoseW_log, des_baseTwistW=p.comTwistW_des_log,
                     baseTwistW=p.comTwistW_log)
-            plotCoM('velocity', 11, time_log=p.time_log.flatten(), des_basePoseW=p.comPoseW_des_log,
+            plotCoM('velocity', 1, time_log=p.time_log.flatten(), des_basePoseW=p.comPoseW_des_log,
                     basePoseW=p.comPoseW_log, des_baseTwistW=p.comTwistW_des_log,
                     baseTwistW=p.comTwistW_log)
             # plotGRFs(2, p.time_log, p.des_forcesW_log, p.grForcesW_log)
-            plotJoint('torque',4, p.time_log.flatten(), p.q_log, p.q_des_log, p.qd_log, p.qd_des_log, None, None, p.tau_log, p.tau_ffwd_log)
+            plotJoint('torque',3, p.time_log.flatten(), p.q_log, p.q_des_log, p.qd_log, p.qd_des_log, None, None, p.tau_log, p.tau_ffwd_log)
 
             plt.figure()
             plt.title('wrench fb')
@@ -149,26 +148,6 @@ if __name__ == '__main__':
             plt.title('wrench gravity')
             plt.plot(p.time_log.T, p.wrench_gW_log.T)
             plt.legend(['Fx', 'Fy', 'Fz', 'Mx', 'My', 'Mz'])
-
-            plt.figure()
-            ax = plt.subplot(3,1,1)
-            ax.plot(p.time_log.T, UK_log)
-            ax.set_title('update kinematics')
-            ax.set_ylabel('comp time [s]')
-            ax = plt.subplot(3,1,2)
-            ax.plot(p.time_log.T, WBC_log)
-            ax.set_title('Whole Body Controller')
-            ax.set_ylabel('comp time [s]')
-            ax = plt.subplot(3, 1, 3)
-            ax.plot(p.time_log.T, pub_log)
-            ax.set_title('Publisher')
-            ax.set_xlabel('iter #')
-            ax.set_ylabel('comp time [s]')
-
-            # for log in [UK_log, WBC_log, pub_log]:
-            #     nonzero = UK_log[np.nonzero(log)]
-            #     avg=np.average(nonzero)
-            #     std = np.std(nonzero)
 
 
 
