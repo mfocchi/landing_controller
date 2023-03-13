5079d227f10b2583a290159fea71164f58a98bfb
froscia <francesco.roscia@iit.it>
2023-03-13 17:32:09 +0000

    bugfix: now wbc works also with yaw!=0
    tested with and without IK reference, with slow.world and fast.world

--- scripts/wbc.py
+++ scripts/wbc.py
@@ -45,10 +45,8 @@ if __name__ == '__main__':
     test['pulse2'] = test['pulse']**2
     try:
 
-        p.startController(world_name='fast.world', use_ground_truth_contacts=False, additional_args=['gui:=true', 'go0_conf:=standDown', 'pid_discrete_implementation:=true'])
-        # p.startController(use_ground_truth_contacts=False,
-        #                   additional_args=['gui:=True', 'go0_conf:=standDown'])
-        # p.startController(additional_args=['gui:=False', 'go0_conf:=standDown'])
+        p.startController(world_name='fast.world', use_ground_truth_contacts=False, additional_args=['gui:=false', 'go0_conf:=standDown', 'pid_discrete_implementation:=true'])
+
         p.startupProcedure()  # overloaded method
 
         if test['typeWBC'] == 'qp':
@@ -72,6 +70,7 @@ if __name__ == '__main__':
             #print(p.time, state)
             # update the kinematics
             p.updateKinematics()
+            # p.visualizeContacts()
             # Reference Generation
             if state == 0:
                 if p.time - time_init < test['usePid']:
@@ -80,6 +79,7 @@ if __name__ == '__main__':
                     state += 1
                     p.x0 = p.comPoseW.copy()
                     p.pid.setPDjoints(p.kp_wbc_j, p.kd_wbc_j, p.ki_wbc_j)
+                    # p.pid.setPDjoints(np.zeros(12),np.zeros(12),np.zeros(12))
                     time_stabilize = p.time.copy()
 
                     p.W_contacts_des = p.W_contacts.copy()
@@ -87,8 +87,7 @@ if __name__ == '__main__':
                     print('starting wbc ' + str(p.time) + " [s]")
             elif state == 1:
                 if p.time - time_stabilize < test['stabilize']:
-                    sigma = (p.time - time_stabilize)/test['stabilize']
-                    p.comPoseW_des = p.x0.copy()
+                    p.comPoseW_des = p.Hframe2World(p.x0)
                     p.comTwistW_des[:] = 0.
                     p.comAccW_des[:] = 0.
 
@@ -107,9 +106,11 @@ if __name__ == '__main__':
             elif state == 2:
                 if p.time - time_sinusoid < test['duration']-(1/4)*(1/test['freq']):
                     # com ref
-                    p.comPoseW_des = p.x0 + test['amp'] * np.sin( test['pulse']*(p.time - time_sinusoid) + test['phase']) -  test['delta']
-                    p.comTwistW_des = test['pulse'] * test['amp'] * np.cos( test['pulse']*(p.time - time_sinusoid) + test['phase'] )
-                    p.comAccW_des = -test['pulse2'] * test['amp'] * np.sin( test['pulse']*(p.time - time_sinusoid) + test['phase'] )
+                    comPoseHF_des = p.x0 + test['amp'] * np.sin( test['pulse']*(p.time - time_sinusoid) + test['phase']) -  test['delta']
+                    comTwistHF_des =  test['pulse'] * test['amp'] * np.cos( test['pulse']*(p.time - time_sinusoid) + test['phase'] )
+                    comAccHF_des  = -test['pulse2'] * test['amp'] * np.sin( test['pulse']*(p.time - time_sinusoid) + test['phase'] )
+
+                    p.comPoseW_des, p.comTwistW_des, p.comAccW_des = p.Hframe2World(comPoseHF_des, comTwistHF_des, comAccHF_des)
 
                     p.Wcom2Joints_des()
                     
@@ -131,9 +132,7 @@ if __name__ == '__main__':
             elif state == 3:
                 if p.time - time_safe_stop < (1/4)*(1/test['freq']):
                     time = p.time - time_safe_stop
-                    p.comPoseW_des = pos(time)
-                    p.comTwistW_des = vel(time)
-                    p.comAccW_des = acc(time)
+                    p.comPoseW_des, p.comTwistW_des, p.comAccW_des = p.Hframe2World(pos(time), vel(time), acc(time))
 
                     p.Wcom2Joints_des()
                     
@@ -182,6 +181,10 @@ if __name__ == '__main__':
             plotContacts('position', time_log=p.time_log, des_LinPose_log=p.B_contacts_des_log, LinPose_log=p.B_contacts_log,
                          contact_states=p.contact_state_log, frame='B', sharex=True, sharey=False, start=0, end=-1)
 
+            plotContacts('position', time_log=p.time_log, des_LinPose_log=p.W_contacts_des_log,
+                         LinPose_log=p.W_contacts_log,
+                         contact_states=p.contact_state_log, frame='W', sharex=True, sharey=False, start=0, end=-1)
+
             # plotContacts('velocity', time_log=p.time_log,  des_LinTwist_log=p.B_vel_contacts_des_log,
             #               frame='B', sharex=True, sharey=False, start=0, end=-1)
 
@@ -196,9 +199,9 @@ if __name__ == '__main__':
             plotJoint('torque', time_log=p.time_log, tau_log=p.tau_log, tau_ffwd_log = p.tau_ffwd_log,
                       tau_des_log=p.tau_fb_log, sharex=True, sharey=False, start=0, end=-1)
 
-            plotWrenches('fb', 8, p.time_log, wrench_fb_log=p.wrench_fbW_log)
-            plotWrenches('ffwd', 9, p.time_log, wrench_ffwd_log=p.wrench_ffW_log)
-            plotWrenches('g', 10, p.time_log, wrench_g_log=p.wrench_gW_log)
+            plotWrenches('fb', 8, p.time_log, des_Wrench_fb_log=p.wrench_fbW_log)
+            plotWrenches('ffwd', 9, p.time_log, des_Wrench_ffwd_log=p.wrench_ffW_log)
+            plotWrenches('g', 10, p.time_log, des_Wrench_g_log=p.wrench_gW_log)
 
             plotFrameLinear('velocity', time_log=p.time_log, des_Twist_log=p.baseTwistW_legOdom_log,
                             Twist_log=p.baseLinTwistImuW_log, title='Base velocity estimate', frame='W', sharex=True,
