915add7dd9756ce84ef48e26cb73118ed64ca157
froscia <francesco.roscia@iit.it>
2023-03-28 10:34:08 +0000

    simulation_id -> simulation_counter. bugfix for store. plots, initconds, data and video are saved at the eand of each run

--- controller/landing_manager.py
+++ controller/landing_manager.py
@@ -29,9 +29,7 @@ class LandingManager:
         return 0
 
 
-    def run(self, simulation_id, basePose_init=None, baseTwist_init=None, useIK=False, useWBC=True, typeWBC='projection', simplified=False):
-
-
+    def run(self, simulation_counter, basePose_init=None, baseTwist_init=None, useIK=False, useWBC=True, typeWBC='projection', simplified=False):
         #########
         # reset #
         #########
@@ -65,6 +63,10 @@ class LandingManager:
                                         smoothing_param = 0.02)
 
             self.lc.setCheckTimings(expected_touch_down_time=self.p.time + (2*self.p.basePoseW[2]-0.3)/self.lc.g_mag , clearance=0.05)
+            if self.settings['VIDEO']['save']:
+                # check if some old jpg are still in /tmp
+                remove_jpg_cmd = "rm /tmp/camera_save/*"
+                os.system(remove_jpg_cmd)
 
             self.p.setGravity(-9.81)
 
@@ -182,6 +184,7 @@ class LandingManager:
                                                                  leg,
                                                                  self.p.legConfig[leg][0],
                                                                  self.p.legConfig[leg][1])
+
                         if isFeasible:
                             self.p.u.setLegJointState(i, q_des_leg, q_des)
 
@@ -232,7 +235,6 @@ class LandingManager:
                                                                                                  self.lc.acc_des)
 
                     if useWBC:
-
                         tau_ffwd = self.p.WBC(self.p.comPoseW_des, self.p.comTwistW_des, comAccW_des, type=typeWBC)
 
 
@@ -247,15 +249,34 @@ class LandingManager:
         if not self.p.real_robot:
             self.p.pause_physics_client()
 
-        if self.settings['PLOTS']['save']:
-            self.settings['SIMS'][simulation_id]['directory'] = self.settings['save_path']+'/sim' + simulation_id
-            os.mkdir(self.settings['SIMS'][simulation_id]['directory'])
-            saveInitConds(self.settings['SIMS'][simulation_id]['directory'],
-                          self.settings['SIMS'][simulation_id],
-                          speedUpDown= self.settings['VIDEO']['speedUpDown'],
-                          verbose=self.settings['verbose'])
+            if self.settings['PLOTS']['save']:
+                self.settings['SIMS'][simulation_counter]['directory'] = self.settings['save_path']+'/sim' + self.settings['SIMS'][simulation_counter]['id']
+                os.mkdir(self.settings['SIMS'][simulation_counter]['directory'])
+                saveInitConds(self.settings['SIMS'][simulation_counter]['directory'],
+                              self.settings['SIMS'][simulation_counter],
+                              speedUpDown= self.settings['VIDEO']['speedUpDown'],
+                              verbose=self.settings['verbose'])
 
-        if self.settings['PLOTS']['save'] or self.settings['PLOTS']['show']:
-            makePlots(self.p, self.settings['FIGURES'], self.settings['PLOTS'], self.settings['verbose'])
+            if self.settings['PLOTS']['save'] or self.settings['PLOTS']['show']:
+                makePlots(p=self.p, figures=self.settings['FIGURES'], PLOT_SETTINGS=self.settings['PLOTS'],
+                          directory=self.settings['SIMS'][simulation_counter]['directory'],
+                          start = 0, end = -1, verbose = self.settings['verbose'])
+
+            # save all for later analysis
+            if self.settings['WORKSPACE']['save']:
+                EXTRADATA = {}
+                EXTRADATA["lift_off"] = self.lc.lc_events.lift_off
+                EXTRADATA["apex"] = self.lc.lc_events.apex
+                EXTRADATA["touch_down"] = self.lc.lc_events.touch_down
+                self.p.saveData(self.settings['SIMS'][simulation_counter]['directory'], EXTRADATA=EXTRADATA,
+                                start=self.lc.lc_events.apex.sample, stop=self.p.log_counter,
+                                verbose = self.settings['verbose'])
+
+            if self.settings['VIDEO']['save']:
+                # save the video
+                self.p.saveVideo(self.settings['SIMS'][simulation_counter]['directory'],
+                                  speedUpDown=self.settings['VIDEO']['speedUpDown'])
 
         return self.returnValue()
+
+
